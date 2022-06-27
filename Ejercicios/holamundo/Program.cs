c7b0a947de2c0b42cd38ee7bb6e556902756da7e
cllg0110 <carollopez0110@gmail.com>
2022-06-26 23:23:53 -0600

    agregando hola mundo

---
+++ Ejercicios/holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace holamundo
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
