4b4b0bdcbd34c531fa230016bb5543fb6ee9dd61
cllg0110 <carollopez0110@gmail.com>
2022-07-27 16:47:55 -0600

    Agregando Tarea 3 Ordenes

---
+++ Ejercicios/10-Ordenes/Cliente.cs
@@ -0,0 +1,12 @@
+public class Cliente: Persona
+{
+   
+    public double Saldo { get; set; }
+
+    public Cliente(int codigo, string nombre, string telefono)
+    {
+        Codigo = codigo;
+        Nombre = nombre;
+        Telefono = telefono;
+    }
+}
